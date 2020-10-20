0e95b19c3381ebe7def4e49dc7ce2f59571b24f3
dev17 <dev17@aiatic.com>
2020-10-20 12:11:21 -0500

    cambios inicio personalizacion

---
+++ appAngular/ClientApp/src/app/mypage/mypage.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-mypage',
+    templateUrl: './mypage.component.html',
+    styleUrls: ['./mypage.component.scss']
+})
+/** mypage component*/
+export class MypageComponent {
+    /** mypage ctor */
+    constructor() {
+
+    }
+}
