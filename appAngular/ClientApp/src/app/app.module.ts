18b2c6a24abd3908bd6bdb85e00e2f08829e0fbe
dev17 <dev17@aiatic.com>
2020-10-20 18:35:48 -0500

    Se añadio el template pero sigue dando errores, uno en styles.css

--- appAngular/ClientApp/src/app/app.module.ts
+++ appAngular/ClientApp/src/app/app.module.ts
@@ -11,6 +11,19 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { MypageComponent } from './mypage/mypage.component';
 
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { LoginComponent } from './examples/login/login.component';
+import { ProfileComponent } from './examples/profile/profile.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,16 +32,35 @@ import { MypageComponent } from './mypage/mypage.component';
     CounterComponent,
     FetchDataComponent,
     MypageComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+
+
+    NgbModule,
+    BrowserAnimationsModule,
+    ComponentsModule,
+    ExamplesModule,
+
+
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+
+      { path: '', redirectTo: 'index', pathMatch: 'full' },
+      { path: 'index',                component: ComponentsComponent },
+      { path: 'nucleoicons',          component: NucleoiconsComponent },
+      { path: 'examples/landing',     component: LandingComponent },
+      { path: 'examples/login',       component: LoginComponent },
+      { path: 'examples/profile',     component: ProfileComponent },
+
+      /*{ path: '', component: HomeComponent, pathMatch: 'full' },      
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'mypage', component: MypageComponent },
+      { path: 'mypage', component: MypageComponent },*/
+
+      { path: '**', component: HomeComponent, pathMatch: 'full' },
     ])
   ],
   providers: [],
